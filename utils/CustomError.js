acc3f177c2201be19d50cb474521af4a3beb75c0
dantecodex <anshulrajput237@gmail.com>
2024-05-31 05:49:25 +0530

    renamed customError.js

--- utils/CustomError.js
+++ utils/CustomError.js
@@ -2,7 +2,7 @@ class CustomError extends Error {
     constructor(message, statusCode) {
         super(message)
         this.statusCode = statusCode;
-        this.status = statusCode >= 400 && statusCode < 500 ? "Failed" : "Server Error"
+        this.status = statusCode >= 400 && statusCode < 500 ? "Failed" : "Server Error";
         Error.captureStackTrace(this, this.construction)
         this.isOperational = true
     }
